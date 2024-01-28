e53643e558d4512fe00ef876e4e2e4ed26641aa8
Artem Ishchenko <blagochestivyj@gmail.com>
2024-01-28 19:20:16 +0300

    Факториал

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,10 @@
 function factorial(n) {
-  // ваш код...
+  let otvet = n;
+  if ( (n == 0) || (n == 1) ) {
+    return 1;
+    } else {
+        for (let i = 1; n - i != 1; i++) {
+            otvet *= (n - i);
+        } return otvet;
+    }
 }
