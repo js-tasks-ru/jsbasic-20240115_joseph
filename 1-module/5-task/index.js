32724a673c41cf30636632e027cf6dcd27876a80
Artem Ishchenko <blagochestivyj@gmail.com>
2024-01-28 20:20:19 +0300

    Модуль 1 и 2

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,13 @@
 function truncate(str, maxlength) {
   // ваш код...
+  let answer = "";
+  if (str.length <= maxlength) {
+    return str;
+  } else {
+    for (let i = 1; i != maxlength; i++) {
+      answer += str[i-1];
+    }
+    return answer + "…"; 
+  }
 }
+  
