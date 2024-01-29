57f50069e5cd39bef92af74a16b0c501497837c5
Artem Ishchenko <blagochestivyj@gmail.com>
2024-01-29 22:32:27 +0300

    Модуль 1 и 2

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -2,5 +2,5 @@ function checkSpam(str) {
   // ваш код...
   let spamWord_1 = "1XBET";
   let spamWord_2 = "XXX";
-  return (str.toUpperCase().includes(spamWord_1) || str.toUpperCase().includes(spamWord_2)) ? true : false;
+  return (str.toUpperCase().includes(spamWord_1) || str.toUpperCase().includes(spamWord_2))
 }
